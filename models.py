c3b86de6c6f04affeb1118058bc62495d44b9b92
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-13 19:40:26 +0100

    Add files via upload

---
+++ models.py
@@ -0,0 +1,23 @@
+
+from sqlalchemy import Column, Integer, String, Float, Date, Enum
+from sqlalchemy.ext.declarative import declarative_base
+import enum
+
+Base = declarative_base()
+
+class EstadoVenda(str, enum.Enum):
+    entregue = "Entregue"
+    por_entregar = "Por entregar"
+    disputa = "Disputa"
+    pago = "Pago"
+
+class ListagemVendas(Base):
+    __tablename__ = "listagem_vendas"
+
+    id = Column(Integer, primary_key=True, index=True)
+    id_venda = Column(Integer, nullable=False)
+    data_evento = Column(Date, nullable=False)
+    evento = Column(String, nullable=False)
+    estadio = Column(String, nullable=False)
+    ganho = Column(Float, nullable=False)
+    estado = Column(Enum(EstadoVenda), nullable=False)
