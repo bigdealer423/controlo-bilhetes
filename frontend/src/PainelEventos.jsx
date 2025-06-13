1b20ecb68834fe32ccff5b3101cb8c0afb99a291
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-13 10:54:01 +0100

    Update PainelEventos.jsx

--- frontend/src/PainelEventos.jsx
+++ frontend/src/PainelEventos.jsx
@@ -1,6 +1,6 @@
-import { Card, CardContent } from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "./components/ui/card";
+import { Badge } from "./components/ui/badge";
+import { Button } from "./components/ui/button";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
