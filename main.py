defe4380979207d21c3f165ed27b598bd4e6e60c
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-13 19:52:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)  # Sem connect_args
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base.metadata.create_all(bind=engine)
