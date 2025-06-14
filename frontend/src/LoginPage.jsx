475544509dd3a998215ffcc629ddef966c3f9859
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-14 09:17:13 +0100

    Update LoginPage.jsx

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
   const autenticar = () => {
     if (utilizador === "bigdealer" && password === "1091") {
       localStorage.setItem("autenticado", "true");
-      navigate("/Dashboard");
+      navigate("/listagem-vendas");
     } else {
       setErro("Credenciais inválidas");
     }
