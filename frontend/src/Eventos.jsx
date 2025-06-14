0d99115da6a53ea9a12a5866a67ab1f1cf6675f2
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-15 00:37:09 +0100

    Update Eventos.jsx

--- frontend/src/Eventos.jsx
+++ frontend/src/Eventos.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";More actions
+import { useEffect, useState } from "react";
 
 export default function Eventos() {
   const [registos, setRegistos] = useState([]);
