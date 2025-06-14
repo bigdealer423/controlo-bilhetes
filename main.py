5cbeefffc0d238ebcf60a79c852c1ea3dc176948
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-14 18:06:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ app = FastAPI()
 # Permitir pedidos do frontend no Vercel
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://controlo-bilhetes.vercel.app"],
+    allow_origins=["https://controlo-bilhetes.vercel.app"]
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
