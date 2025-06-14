ebcbed75d34a7f98dfed96fc26ff884a2b71685e
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-14 09:41:06 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import LoginPage from "./LoginPage";
 import Dashboard from "./Dashboard";
 import ListagemVendas from "./ListagemVendas";
@@ -10,14 +10,7 @@ import Compras from "./Compras";
 import Outro from "./Outro";
 
 export default function App() {
-  const [autenticado, setAutenticado] = useState(false);
-
-  useEffect(() => {
-    const autenticadoLocal = localStorage.getItem("autenticado");
-    setAutenticado(autenticadoLocal === "true");
-  }, []);
-
-  if (autenticado === null) return null; // evitar mostrar rota antes do estado carregar
+  const [autenticado, setAutenticado] = useState(localStorage.getItem("autenticado") === "true");
 
   return (
     <Router>
@@ -32,49 +25,49 @@ export default function App() {
         <Route
           path="/dashboard"
           element={
-            autenticado ? <Dashboard /> : <Navigate to="/" replace />
+            autenticado ? <Dashboard setAutenticado={setAutenticado} /> : <Navigate to="/" replace />
           }
         />
 
         <Route
           path="/listagem-vendas"
           element={
-            autenticado ? <><Dashboard /><ListagemVendas /></> : <Navigate to="/" replace />
+            autenticado ? <><Dashboard setAutenticado={setAutenticado} /><ListagemVendas /></> : <Navigate to="/" replace />
           }
         />
 
         <Route
           path="/eventos"
           element={
-            autenticado ? <><Dashboard /><Eventos /></> : <Navigate to="/" replace />
+            autenticado ? <><Dashboard setAutenticado={setAutenticado} /><Eventos /></> : <Navigate to="/" replace />
           }
         />
 
         <Route
           path="/info-clubes"
           element={
-            autenticado ? <><Dashboard /><InfoClubes /></> : <Navigate to="/" replace />
+            autenticado ? <><Dashboard setAutenticado={setAutenticado} /><InfoClubes /></> : <Navigate to="/" replace />
           }
         />
 
         <Route
           path="/disputas"
           element={
-            autenticado ? <><Dashboard /><Disputas /></> : <Navigate to="/" replace />
+            autenticado ? <><Dashboard setAutenticado={setAutenticado} /><Disputas /></> : <Navigate to="/" replace />
           }
         />
 
         <Route
           path="/compras"
           element={
-            autenticado ? <><Dashboard /><Compras /></> : <Navigate to="/" replace />
+            autenticado ? <><Dashboard setAutenticado={setAutenticado} /><Compras /></> : <Navigate to="/" replace />
           }
         />
 
         <Route
           path="/outro"
           element={
-            autenticado ? <><Dashboard /><Outro /></> : <Navigate to="/" replace />
+            autenticado ? <><Dashboard setAutenticado={setAutenticado} /><Outro /></> : <Navigate to="/" replace />
           }
         />
 
