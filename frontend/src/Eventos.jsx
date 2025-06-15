6556a9ae13ec858c5aa5cd4313972dfcc8596fb6
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-15 16:17:25 +0100

    Update Eventos.jsx

--- frontend/src/Eventos.jsx
+++ frontend/src/Eventos.jsx
@@ -143,9 +143,7 @@ useEffect(() => {
           <tbody>
             {registos.map(r => (
               <>
-                <tr key={r.id}>
-                <tr
-                    key={r.id}className={`cursor-pointer ${r.estado === "Pago" ? "bg-green-100" : ""}`}>
+                <trkey={r.id}className={`cursor-pointer ${r.estado === "Pago" ? "bg-green-100" : ""}`}>
                   <td className="p-2">
                     <button onClick={() => setLinhaExpandida(linhaExpandida === r.id ? null : r.id)}>
                       {linhaExpandida === r.id ? "🔼" : "🔽"}
