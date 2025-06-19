d3fb0c0bc0c417c752532cccbcbdc05a084a0f57
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-19 14:11:49 +0100

    Update ListagemVendas.jsx

--- frontend/src/ListagemVendas.jsx
+++ frontend/src/ListagemVendas.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { toast } from "react-toastify"
+import { toast } from "react-toastify";
 
 export default function ListagemVendas(props) {
   const [registos, setRegistos] = useState([]);
