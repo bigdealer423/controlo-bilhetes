769aa93912e27602f011567d8ed87d7d78d968b7
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-19 15:57:06 +0100

    Update Dashboard.jsx

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -87,5 +87,5 @@ export default function Dashboard({ onAtualizarEventos }) {
       fechar={() => setMostrarModal(false)}  // Garante que o modal pode ser fechado
       onAtualizar={onAtualizarEventos}
     />
-  </div>
+    </div>
 );
