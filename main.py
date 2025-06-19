6486b249aaeff22e989d10bf92e9b6cb40dfb816
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-19 16:27:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://controlo-bilhetes.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
