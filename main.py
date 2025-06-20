afa67e00f21863e0d9bff2fa202f79cc2059c759
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-20 13:06:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -51,11 +51,11 @@ def root():
 @app.get("/disputas", response_model=List[ListagemVendasBase])
 def get_disputas(db: Session = Depends(get_db)):
     print("CORS Test - Requisição recebida")
-    # Consulta para pegar apenas as vendas com estado 'Disputa'
     disputas = db.query(ListagemVendas).filter(ListagemVendas.estado == 'Disputa').all()
     if not disputas:
         raise HTTPException(status_code=404, detail="Nenhuma disputa encontrada")
-    return disputas  # Aqui, o FastAPI vai converter a lista de objetos SQLAlchemy para Pydantic
+    return disputas
+
 
 
 
