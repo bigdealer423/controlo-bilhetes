7fa7d99a02fc98d7432b478c6885605f006533d9
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-20 14:07:40 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, Date, Enum
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Date, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from pydantic import BaseModel
 from datetime import date
