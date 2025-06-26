14df6c8cd1daa8829fe0d0ad71dc5287e776cf45
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-26 20:31:48 +0100

    Update Eventos.jsx

--- frontend/src/Eventos.jsx
+++ frontend/src/Eventos.jsx
@@ -529,14 +529,14 @@ return (
 </td>
         <td className="p-2">{c.quantidade}</td>
         <td className="p-2">{c.gasto} €</td>
-        <td className="p-2">
-      <CirculoEstado
-      tipo="compras"
-      id={c.id}
-      texto_estado={c.circulo_estado_compra}
-      nota_estado={c.nota_estado_compra}
-      setCompras={setCompras}
-    />
+        <td className="p-2" colSpan="2">
+  <CirculoEstado
+    tipo="listagem_vendas"
+    id={v.id}
+    texto_estado={v.circulo_estado_venda}
+    nota_estado={v.nota_estado_venda}
+    setVendas={setVendas}
+  />
 </td>
         <td className="p-2" colSpan="3">
           <button onClick={() => { setModoEdicaoCompra(c.id); setCompraEditada(c); }} className="text-blue-600 hover:underline">
