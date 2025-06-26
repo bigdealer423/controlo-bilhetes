11ead93a3f65130c1b285460fbed4f589f90b315
bigdealer423 <miguelitocosta423@gmail.com>
2025-06-26 20:38:38 +0100

    Update Eventos.jsx

--- frontend/src/Eventos.jsx
+++ frontend/src/Eventos.jsx
@@ -459,20 +459,20 @@ return (
 </td>
         <td className="p-2">{v.ganho} €</td>
         <td className="p-2 whitespace-nowrap">{v.estado}</td>
-        <td className="p-2">
-              <CirculoEstado
-              tipo="listagem_vendas"
-              id={v.id}
-              texto_estado={v.circulo_estado_venda}
-              nota_estado={v.nota_estado_venda}
-              setVendas={setVendas}
-            />
-          </td>
-        <td className="p-2" colSpan="4">
-          <button onClick={() => { setModoEdicaoVenda(v.id); setVendaEditada(v); }} className="text-blue-600 hover:underline">
-            Editar
-          </button>
-        </td>
+        <td className="p-2" colSpan="2">
+        <CirculoEstado
+          tipo="listagem_vendas"
+          id={v.id}
+          texto_estado={v.circulo_estado_venda}
+          nota_estado={v.nota_estado_venda}
+          setVendas={setVendas}
+        />
+      </td>
+      <td className="p-2" colSpan="2">
+        <button onClick={() => { setModoEdicaoVenda(v.id); setVendaEditada(v); }} className="text-blue-600 hover:underline">
+          Editar
+        </button>
+      </td>
       </>
     )}
   </tr>
