ea7ddeecfc48c4bb9c63a2eea6b2255374cf5b04
bigdealer423 <miguelitocosta423@gmail.com>
2025-07-04 18:25:38 +0100

    Create clubesLinks.js

---
+++ frontend/src/clubesLinks.js
@@ -0,0 +1,21 @@
+// clubesLinks.js
+const clubesLinks = [
+  {
+    nome: "SL Benfica",
+    logo: "/logos/slb.svg",
+    urlVenda: "https://www.slbenfica.pt/pt-pt/bilhetes",
+  },
+  {
+    nome: "FC Porto",
+    logo: "/logos/fcp.svg",
+    urlVenda: "https://www.fcporto.pt/pt/bilhetes",
+  },
+  {
+    nome: "Sporting CP",
+    logo: "/logos/scp.svg",
+    urlVenda: "https://www.sporting.pt/pt/bilhetes",
+  },
+  // Adiciona mais clubes facilmente aqui
+];
+
+export default clubesLinks;
