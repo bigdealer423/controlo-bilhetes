d8d0d05d46a8ca1f9532a0c62d3bf1a964d1bc93
bigdealer423 <miguelitocosta423@gmail.com>
2025-07-13 17:21:03 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-python -m playwright install --with-deps chromium
+# Instala o Chromium diretamente na pasta do projeto
+python -m playwright install chromium --target=/opt/render/project/.playwright
+
+# Define a variável de ambiente para usar o caminho correto
+export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/.playwright
+
+# Corre a tua app
 python main.py
