7eefbcb15f177bcdd277a436fd8a8652fc7dbc66
bigdealer423 <miguelitocosta423@gmail.com>
2025-07-13 17:46:53 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
+
+# Força a instalação de Python 3.10
+pyenv install 3.10.13
+pyenv global 3.10.13
+
+# Garante que o pip está atualizado
+pip install --upgrade pip
+
+# Instala as dependências
 pip install -r requirements.txt
+
+# Instala os browsers do Playwright no local correto
+python -m playwright install chromium --target=/opt/render/project/.playwright
