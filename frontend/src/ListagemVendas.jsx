ea2a6c4afcf467232f2898f2bfa896e2f35b0fb6
bigdealer423 <miguelitocosta423@gmail.com>
2025-07-16 18:52:33 +0100

    Update ListagemVendas.jsx

--- frontend/src/ListagemVendas.jsx
+++ frontend/src/ListagemVendas.jsx
@@ -418,7 +418,7 @@ const [ordemAscendente, setOrdemAscendente] = useState(false);
         </div>
       
   
-      <div className="overflow-x-auto w-full">
+      <div className="hidden md:block overflow-x-auto w-full">
         <table className="min-w-full border border-gray-300 dark:border-gray-600 text-sm text-left text-gray-900 dark:text-gray-100 transition-colors duration-300">
          <thead className="bg-gray-100 dark:bg-gray-800 transition-colors duration-300">
           <tr>
