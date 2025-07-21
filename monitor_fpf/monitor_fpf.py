e660dad760af3de9b46f9f1ca629988296d6de58
bigdealer423 <miguelitocosta423@gmail.com>
2025-07-21 11:54:19 +0100

    Update monitor_fpf.py

--- monitor_fpf/monitor_fpf.py
+++ monitor_fpf/monitor_fpf.py
@@ -32,7 +32,7 @@ SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
 
 PALAVRAS_CHAVE_FPF = ["Comprar", "Adquirir", "Bilhete", "Ingressos", "Buy", "IRL", "HUN", "Irlanda", "Hungria"]
-PALAVRAS_CHAVE_SLB = ["Carcavelos", "Fatima", "17ª Jornada", "18ª Jornada"]
+PALAVRAS_CHAVE_SLB = ["Carcavelos", "Fatima", "17ª Jornada", "18ª Jornada", "Nice"]
 PALAVRAS_CHAVE_SPORTING = ["comprar bilhetes"]
 PALAVRAS_CHAVE_BLUETICKET = ["Benfica"]
 
