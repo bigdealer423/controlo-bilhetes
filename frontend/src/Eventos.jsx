8843b48adcf180a6748fc68e532a2da50222a56e
bigdealer423 <miguelitocosta423@gmail.com>
2025-07-26 19:39:20 +0100

    Update Eventos.jsx

--- frontend/src/Eventos.jsx
+++ frontend/src/Eventos.jsx
@@ -461,7 +461,7 @@ return (
   />
 
   {/* ✅ Botão "Ocultar Pagos" apenas no mobile */}
-  <div className="xl:hidden mt-2 flex justify-end">
+  <div className="lg:hidden mt-2 flex justify-end">
     <button
       onClick={() => setOcultarPagos(!ocultarPagos)}
       className={`px-3 py-2 rounded text-white text-sm font-semibold transition-colors ${
