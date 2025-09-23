a684eedea43c1f47249b493bdb49124807730c36
bigdealer423 <miguelitocosta423@gmail.com>
2025-09-23 23:20:04 +0100

    Update Eventos.jsx

--- frontend/src/Eventos.jsx
+++ frontend/src/Eventos.jsx
@@ -586,7 +586,7 @@ const imprimirVendasComNotaVermelha = (vendasDoEvento, tituloEvento = "Vendas co
 
 
 return (
-   <div className="p-4 md:p-6 w-full md:max-w-8xl md:mx-auto min-h-screen">
+   <div className="p-4 md:p-6 w-full md:max-w-[1400px] md:mx-auto min-h-screen">
       <h1 className="text-2xl font-bold mb-4 text-gray-900 dark:text-gray-100">Resumo de Eventos</h1>
 
 
