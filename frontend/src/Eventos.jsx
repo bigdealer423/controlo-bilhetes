7dbe09f578d1c868dd05703be26c3604d89e6d0b
bigdealer423 <miguelitocosta423@gmail.com>
2025-10-11 09:54:14 +0100

    Update Eventos.jsx

--- frontend/src/Eventos.jsx
+++ frontend/src/Eventos.jsx
@@ -1566,7 +1566,7 @@ return (
           <img
             src={viagogoLogo}
             alt="Viagogo"
-            className="w-8 h-5 inline-block align-[-2px] ml-1"
+            className="w-5 h-5 inline-block align-[-2px]"
             loading="lazy"
           />
         </a>
