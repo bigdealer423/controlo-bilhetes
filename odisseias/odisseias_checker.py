bc77c75b40aab2892d2a9be5a8eb23dd6f67d001
bigdealer423 <miguelitocosta423@gmail.com>
2026-01-28 23:30:10 +0000

    Update odisseias_checker.py

--- odisseias/odisseias_checker.py
+++ odisseias/odisseias_checker.py
@@ -11,7 +11,7 @@ STORAGE_STATE = "/opt/render/project/src/odisseias/storage_state.json"
 subprocess.run(["playwright", "install", "chromium"], check=True)
 
 # ---- CONFIGURAÇÕES ----
-PALAVRAS_CHAVE = ["fenerbahçe", "benfica", "sporting", "porto"]
+PALAVRAS_CHAVE = ["alverca", "benfica", "sporting", "porto"]
 PRODUTOS_URL = "https://www.odisseias.com/Book/ProductList"
 
 # Email de alerta
