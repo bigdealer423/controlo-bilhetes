f84cd174019cdb495ba5f8df687216a9dbd79d03
bigdealer423 <miguelitocosta423@gmail.com>
2026-03-17 14:48:20 +0000

    Update odisseias_esgotado_checker.py

--- odisseias_esgotado/odisseias_esgotado_checker.py
+++ odisseias_esgotado/odisseias_esgotado_checker.py
@@ -9,7 +9,7 @@ from email.mime.application import MIMEApplication
 
 URLS = [
     #"https://viagens.slbenfica.pt/programas/sporting-cp-vs-sl-benfica-30-jornada-com-almoco/1005818#ps:777b9859-48aa-4ca9-89e5-4f85fd585cf3",
-    "https://viagens.slbenfica.pt/programas/casa-pia-ac-vs-sl-benfica-28-jornada-com-almoco/1005817#ps:c43f8dbd-6376-4c13-99a3-d514153e9b78",
+    #"https://viagens.slbenfica.pt/programas/casa-pia-ac-vs-sl-benfica-28-jornada-com-almoco/1005817#ps:c43f8dbd-6376-4c13-99a3-d514153e9b78",
 ]
 
 SELECTOR_COOKIES = "button.btn.btn-secondary.js_cookie_banner_accept_btn"
